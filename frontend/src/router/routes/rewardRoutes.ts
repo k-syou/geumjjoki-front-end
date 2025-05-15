c589d6b208ea99018e1be28bb6fc82967678b758
jm13 <ktd562@naver.com>
2025-05-15 17:47:59 +0900

    feat:리워드 메인페이지 및 구매내역페이지 구현

--- frontend/src/router/routes/rewardRoutes.ts
+++ frontend/src/router/routes/rewardRoutes.ts
@@ -1,13 +1,26 @@
 import MobileLayout from '@/layouts/MobileLayout.vue'
 import RewardView from '@/views/RewardView.vue'
+import Reward_01 from '@/components/pages/reward/Reward_01.vue'
+import Reward_02 from '@/components/pages/reward/Reward_02.vue'
 
 export const rewardRoutes = [
   {
     path: '/reward',
-    name: 'reward',
     component: RewardView,
     meta: {
       layout: MobileLayout,
     },
+    children: [
+      {
+        path: '',
+        name: 'reward',
+        component: Reward_01,
+      },
+      {
+        path: 'pay',
+        name: 'reward_02',
+        component: Reward_02,
+      },
+    ],
   },
 ]
